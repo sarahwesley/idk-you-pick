342e3dec7dfd1b59da3550411d5590d8ce51ca78
CTripleF <cjfabra1@gmail.com>
2021-11-29 16:55:39 -0500

    properly passing information from zip to restaurant API and storing results for display use

--- assets/js/results.js
+++ assets/js/results.js
@@ -1,6 +1,8 @@
 // pull saved zip from local storage
 var pullZipCode = JSON.parse(localStorage.getItem("zipCode"));
 var pulledCity = "";
+var returnedCities;
+var cityWWRid;
 // run zip through ziptastic api
 //zip api
 var userZip = {
@@ -16,10 +18,20 @@ var userZip = {
 $.ajax(userZip).done(function (cityInfo) {
 	localStorage.setItem("cityName", JSON.stringify(cityInfo)); // saves informations about city location from zipcode entered in local
 	var cityObj = JSON.parse(cityInfo); // converts string back to object
-	//console.log(pulledCity);
+	console.log(cityObj);
 	pulledCity = cityObj.city; // use key 'city' from within object to pull info
-	console.log('your city is ' + pulledCity)
+	console.log('your city is ' + pulledCity);
+	getCityWWRInfo();
+	
 });
+
+// function getCityID (){
+// 	for (i = 0; i< returnedCities.length(); i++) {
+// 		if(returnedCities[i].result_object == `validation parameter`) {
+//			break;
+// 		}
+// 	}
+// };
 
 // get from local storage
 
@@ -27,50 +39,59 @@ $.ajax(userZip).done(function (cityInfo) {
 // write a function to see if pullZipCode and passCiy match location_id and name
 
 
-// wordlwide restaurant results 
-// const results = {
-// 	"async": true,
-// 	"crossDomain": true,
-// 	"url": "https://worldwide-restaurants.p.rapidapi.com/search",
-// 	"method": "POST",
-// 	"headers": {
-// 		"content-type": "application/x-www-form-urlencoded",
-// 		"x-rapidapi-host": "worldwide-restaurants.p.rapidapi.com",
-// 		"x-rapidapi-key": "161a4fc731mshd1f703035034458p1b1d71jsn4fa88ad04868"
-// 	},
-// 	"data": {
-// 		"language": "en_US",
-// 		"limit": "15", // # of results returned in array
-// 		"location_id": "34600", // change ID # to specify area
-// 		"currency": "USD"
-// 	}
-// };
+// wordlwide restaurant results
+function getRestaurants() {
+	const results = {
+		"async": true,
+		"crossDomain": true,
+		"url": "https://worldwide-restaurants.p.rapidapi.com/search",
+		"method": "POST",
+		"headers": {
+			"content-type": "application/x-www-form-urlencoded",
+			"x-rapidapi-host": "worldwide-restaurants.p.rapidapi.com",
+			"x-rapidapi-key": "161a4fc731mshd1f703035034458p1b1d71jsn4fa88ad04868"
+		},
+		"data": {
+			"language": "en_US",
+			"limit": "15", // # of results returned in array
+			"location_id": cityWWRid, // change ID # to specify area
+			"currency": "USD"
+		}
+	};
+	$.ajax(results).done(function (response) {
+		console.log(response);
+	});
+};
 
-const cityIdentification = {
-	"async": true,
-	"crossDomain": true,
-	"url": "https://worldwide-restaurants.p.rapidapi.com/typeahead",
-	"method": "POST",
-	"headers": {
-		"content-type": "application/x-www-form-urlencoded",
-		"x-rapidapi-host": "worldwide-restaurants.p.rapidapi.com",
-		"x-rapidapi-key": "161a4fc731mshd1f703035034458p1b1d71jsn4fa88ad04868"
-	},
-	"data": {
-		"q": pulledCity, // use city name to get location ID #
-		"language": "en_US"
-	}
+function getCityWWRInfo(){
+	const cityIdentification = {
+		"async": true,
+		"crossDomain": true,
+		"url": "https://worldwide-restaurants.p.rapidapi.com/typeahead",
+		"method": "POST",
+		"headers": {
+			"content-type": "application/x-www-form-urlencoded",
+			"x-rapidapi-host": "worldwide-restaurants.p.rapidapi.com",
+			"x-rapidapi-key": "161a4fc731mshd1f703035034458p1b1d71jsn4fa88ad04868"
+		},
+		"data": {
+			"q": pulledCity, // use city name to get location ID #
+			"language": "en_US"
+		}
+	};
+
+	// console log info from results and cityIdentifier for testing purposes
+	$.ajax(cityIdentification).done(function (response) {
+		returnedCities = response.results.data;
+		console.log(returnedCities);
+		//console.log(returnedCities[0].result_object.location_id)
+		cityWWRid = returnedCities[0].result_object.location_id; // will need to move after validation created
+		console.log(cityWWRid)
+		getRestaurants();
+	});
 };
 
 
-// console log info from results and cityIdentifier for testing purposes
-$.ajax(cityIdentification).done(function (response) {
-	console.log(response);
-});
-
-$.ajax(results).done(function (response) {
-	console.log(response);
-});
 
 
 // function to grab location based on zip that is entered. 
