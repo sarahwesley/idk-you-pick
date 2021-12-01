d17924f21db700b278d3565d517600bda3b69100
sarahwesley <92330391+sarahwesley@users.noreply.github.com>
2021-11-30 20:32:27 -0500

    removed unused function

--- assets/js/results.js
+++ assets/js/results.js
@@ -93,18 +93,6 @@ function getCityWWRInfo() {
 	});
 };
 
-/////// Random Function maybe?? :)
-// class GenerateRandom {
-   // public static void main( String args[] ) {
-     // int min = 50;
-      //int max = 100;
-        
-      //Generate random int value from 50 to 100 
-      //System.out.println("Random value in int from "+min+" to "+max+ ":");
-      //int random_int = (int)Math.floor(Math.random()*(max-min+1)+min);
-     // System.out.println(random_int);
-    // }
-// }
 
 
 // function to grab location based on zip that is entered. 
